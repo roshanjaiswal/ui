59fd4df074c3cf10e3284965aba331eafcb7baf5
Roshan K Jaiswal <rosjaisw@in.ibm.com>
2021-09-05 20:56:25 +0530

    my changes

--- myLearning/notes.js
+++ myLearning/notes.js
@@ -146,8 +146,8 @@ eval("var x;")
 
 /* Falsy
     A falsy value is something which evaluates to FALSE, for instance when checking a variable. 
-  There are only six falsey values in JavaScript: 
-  undefined, null, NaN, 0, "" (empty string), and false of course
+    There are only six falsey values in JavaScript: 
+    undefined, null, NaN, 0, "" (empty string), and false of course
 */
 
 if ([] == false) // <-- truthy, will run code in if-block
@@ -186,8 +186,36 @@ for (i = 0; i < 3; i++) {
 //   "i = 1, j = 0"
 //   "i = 2, j = 0"
 //   "i = 2, j = 1"
+//------------------------------------------------------------------------------------------------------------------------------------------------
+Array.from();
 
-//----------------------------------------------------------------------------------------------------------------------
+// Using an arrow function as the map function to manipulate the elements
+Array.from([1, 2, 3], x => x + x);
+// [2, 4, 6]
+
+// Generate a sequence of numbers
+// Since the array is initialized with `undefined` on each position,
+// the value of `v` below will be `undefined`
+Array.from({length: 5}, (v, i) => i);
+// [0, 1, 2, 3, 4]
+
+
+// Sequence generator function (commonly referred to as "range", e.g. Clojure, PHP etc)
+const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
+
+// Generate numbers range 0..4
+range(0, 4, 1);
+// [0, 1, 2, 3, 4]
+
+// Generate numbers range 1..10 with step of 2
+range(1, 10, 2);
+// [1, 3, 5, 7, 9]
+
+// Generate the alphabet using Array.from making use of it being ordered as a sequence
+range('A'.charCodeAt(0), 'Z'.charCodeAt(0), 1).map(x => String.fromCharCode(x));
+// ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+//------------------------------------------------------------------------------------------------------------------
 /*
 What are regular expression patterns
 -----------------------------------
